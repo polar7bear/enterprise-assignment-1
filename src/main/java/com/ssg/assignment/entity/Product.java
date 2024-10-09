b41fab27d748e9696b78290c34bc58b9b78f4f37
polar7bear <sons1998@naver.com>
2024-10-09 12:54:45 +0900

    Add: 엔티티 설계

---
+++ src/main/java/com/ssg/assignment/entity/Product.java
@@ -0,0 +1,24 @@
+package com.ssg.assignment.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private float score;
+
+    @OneToMany(mappedBy = "product")
+    List<Review> reivews = new ArrayList<>();
+}
---
+++ src/main/java/com/ssg/assignment/entity/Review.java
@@ -0,0 +1,33 @@
+package com.ssg.assignment.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private float reviewScore;
+
+    private String content;
+
+    private String imageUrl;
+
+    private Long userId;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+}
