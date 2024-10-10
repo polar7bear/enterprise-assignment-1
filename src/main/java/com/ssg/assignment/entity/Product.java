f644c52173339b97a457665f55af189e64c379aa
polar7bear <sons1998@naver.com>
2024-10-10 10:11:20 +0900

    Fix: not null 컬럼 수정

--- src/main/java/com/ssg/assignment/entity/Product.java
+++ src/main/java/com/ssg/assignment/entity/Product.java
@@ -14,7 +14,11 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private long reviewCount;
+
+    @Column(nullable = false)
     private float score;
 
     @OneToMany(mappedBy = "product")
